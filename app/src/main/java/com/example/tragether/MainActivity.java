30297680be7f169716b775fbcd366b962ad60aa7
Greta Rossetto <greta@datatellers.info>
2019-06-11 18:10:31 +0200

    work in progress auth

--- app/src/main/java/com/example/tragether/MainActivity.java
+++ app/src/main/java/com/example/tragether/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.tragether;
 
+import android.app.AlertDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,28 +18,153 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
+import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.Api;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.FileDescriptor;
+import java.io.PrintWriter;
+import java.util.concurrent.TimeUnit;
 
-    private FirebaseAuth mAuth;
+import dmax.dialog.SpotsDialog;
+
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
+
+    private static final int PERMISSION_SIGN_IN = 1;
+    GoogleApiClient mGoogleApiClient;
+    FirebaseAuth mAuth;
+    SignInButton signInButton;
+
+    AlertDialog waiting_dialog;
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == PERMISSION_SIGN_IN){
+
+            waiting_dialog.show();
+
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+
+            if(result.isSuccess()){
+
+                waiting_dialog.dismiss();
+
+                GoogleSignInAccount account = result.getSignInAccount();
+                String idToken = account.getIdToken();
+
+                AuthCredential credential = GoogleAuthProvider.getCredential(idToken,null);
+                firebaseAuthWithGoogle(credential);
+
+            }else{
+
+                waiting_dialog.dismiss();
+
+                Log.e("EDMT_ERROR", "Login failed");
+                Log.e("EDMT_ERROR", result.getStatus().getStatusMessage());
+
+            }
+
+        }
+    }
+
+    private void firebaseAuthWithGoogle(AuthCredential credential) {
+
+        mAuth.signInWithCredential(credential)
+                .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                    @Override
+                    public void onSuccess(AuthResult authResult) {
+                        //start new activity and pass email to the new activity
+                        Intent intent = new Intent(MainActivity.this, logged_activity.class);
+                        intent.putExtra("email", authResult.getUser().getEmail());
+                        startActivity(intent);
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(MainActivity.this, ""+e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        configureGoogleSignIn();
+
+        mAuth = FirebaseAuth.getInstance();
+        signInButton = (SignInButton)findViewById(R.id.sign_in_button);
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signIn();
+            }
+        });
+
+        waiting_dialog = new SpotsDialog.Builder().setContext(this)
+                .setMessage("Please wait...")
+                .setCancelable(false)
+                .build();
 
 
         mAuth = FirebaseAuth.getInstance();
        // https://www.youtube.com/watch?v=4h4y4mnJIBs
 
     }
+
+    private void signIn() {
+        Intent intent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(intent, PERMISSION_SIGN_IN);
+
+
+    }
+
+    private void configureGoogleSignIn() {
+        GoogleSignInOptions options = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this,this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, options)
+                .build();
+
+        mGoogleApiClient.connect();
+
+    }
+
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+        Toast.makeText(this, ""+connectionResult.getErrorMessage(), Toast.LENGTH_SHORT).show();
+
+    }
+
     //need later to move on if already logged in
     @Override
     public void onStart(){
@@ -46,8 +177,7 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-
-   // @SuppressWarnings("StatementWithEmptyBody")
+    // @SuppressWarnings("StatementWithEmptyBody")
 
 
 }
