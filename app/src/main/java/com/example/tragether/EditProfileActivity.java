ccf557a61ae48d813de314755e5bbb43383004b0
Greta Rossetto <greta.rossetto93@gmail.com>
2019-06-26 15:49:18 +0200

    Added country spinner and constraints

--- app/src/main/java/com/example/tragether/EditProfileActivity.java
+++ app/src/main/java/com/example/tragether/EditProfileActivity.java
@@ -4,8 +4,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Locale;
 
 public class EditProfileActivity extends AppCompatActivity {
+
+    Spinner countries;
+    ArrayList<String> countriesArray = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +23,29 @@ public class EditProfileActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         setContentView(R.layout.activity_edit_profile);
+
+        countries = (Spinner) findViewById(R.id.countriesSpinner);
+        Locale[] locale = Locale.getAvailableLocales();
+
+        String country;
+        for( Locale loc : locale ){
+            country = loc.getDisplayCountry();
+            if( country.length() > 0 && !countriesArray.contains(country) ){
+                countriesArray.add( country );
+            }
+        }
+
+        Collections.sort(countriesArray, String.CASE_INSENSITIVE_ORDER);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item, countriesArray);
+        countries.setAdapter(adapter);
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
 
 
     }
