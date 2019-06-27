aa5a97d869767c9234a71d2bd037fc519ce9ea6c
Greta Rossetto <greta.rossetto93@gmail.com>
2019-06-27 11:29:36 +0200

    second merge, it probably works

--- app/src/main/java/com/example/tragether/ProfileActivity.java
+++ app/src/main/java/com/example/tragether/ProfileActivity.java
@@ -1,13 +1,7 @@
 package com.example.tragether;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 
 import android.view.View;
 
@@ -18,8 +12,6 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
-
-import org.w3c.dom.Text;
 
 public class ProfileActivity  extends  MenuHandler {
 
@@ -45,7 +37,7 @@ public class ProfileActivity  extends  MenuHandler {
 
         greetingsProfile = (TextView)findViewById(R.id.greetingsProfile);
         greetingsProfile.setText("Hello " + FirebaseAuth.getInstance().getCurrentUser().getEmail());
-        editProfile = (Button) findViewById(R.id.btnEditProfile);
+        editProfile = (Button) findViewById(R.id.btnGoToProfile);
         editProfile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
--- app/src/main/java/com/example/tragether/logged_activity.java
+++ app/src/main/java/com/example/tragether/logged_activity.java
@@ -1,12 +1,8 @@
 package com.example.tragether;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
@@ -60,9 +56,9 @@ public class logged_activity extends  MenuHandler {
             }
         });
 
-        goToProfile = findViewById(R.id.btnProfile);
+        goToProfile = findViewById(R.id.btnGoToProfile);
 
-        goToProfile = (Button)findViewById(R.id.btnEditProfile);
+        goToProfile = (Button)findViewById(R.id.btnGoToProfile);
 
         goToProfile.setOnClickListener(new View.OnClickListener() {
             @Override
