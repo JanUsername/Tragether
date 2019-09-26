3b2412ede31ae731b0c235546e6c04631cce8ae1
Greta Rossetto <greta.rossetto93@gmail.com>
2019-09-26 14:31:58 +0200

    :rooster: time picker works on first click

--- app/src/main/java/com/example/tragether/database/EventDao.java
+++ app/src/main/java/com/example/tragether/database/EventDao.java
@@ -4,11 +4,22 @@ import androidx.room.*;
 
 import com.example.tragether.model.Event;
 
+import java.util.ArrayList;
+
 @Dao
 public interface EventDao {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     void insert(Event event);
+
+    @Query("SELECT * FROM events_table")
+    ArrayList<Event> loadEvents();
+
+    @Update(onConflict = OnConflictStrategy.REPLACE)
+    void update(Event event);
+
+    @Delete
+    void delete(Event event);
 
 
 }
--- app/src/main/java/com/example/tragether/database/TravelDao.java
+++ app/src/main/java/com/example/tragether/database/TravelDao.java
@@ -5,23 +5,20 @@ import androidx.room.*;
 import com.example.tragether.model.Travel;
 
 import java.util.ArrayList;
-import java.util.Date;
 
 @Dao
 public interface TravelDao {
 
-   /* @Query("SELECT * FROM user_table")
-    ArrayList<Travel> loadTravels();
-
-    @Update(onConflict = OnConflictStrategy.REPLACE)
-    void update(Travel travel);
-
-    @Delete
-    void delete(Travel user);
-
-    @Query("SELECT timestamp FROM user_table WHERE email LIKE :uEmail")
-    Date getTimestamp(String uEmail);
-*/
    @Insert(onConflict = OnConflictStrategy.REPLACE)
    void insert(Travel travel);
+
+   @Query("SELECT * FROM travels_table")
+   ArrayList<Travel> loadTravels();
+
+   @Update(onConflict = OnConflictStrategy.REPLACE)
+   void update(Travel travel);
+
+   @Delete
+   void delete(Travel travel);
+
 }
