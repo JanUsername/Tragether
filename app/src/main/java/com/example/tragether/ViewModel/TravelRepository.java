993f66046192070e19a6bb549a826d1cb8813383
jan.schmidniederkofler@stud-inf.unibz.it <jan.schmidniederkofler@stud-inf.unibz.it>
2019-10-06 04:15:48 +0200

    Added the viewModel for the fragments. They work TODO for logged activity, and insert/delete

---
+++ app/src/main/java/com/example/tragether/ViewModel/TravelRepository.java
@@ -0,0 +1,94 @@
+package com.example.tragether.ViewModel;
+
+import android.app.Application;
+import android.os.AsyncTask;
+
+import com.example.tragether.database.SupportDataBase;
+import com.example.tragether.database.TravelDao;
+import com.example.tragether.model.Travel;
+import com.example.tragether.model.Utility;
+
+import java.util.List;
+
+
+public class TravelRepository {
+    private TravelDao travelDao;
+
+    private List<Travel> allTravels;
+
+    public TravelRepository(Application application) {
+
+
+        allTravels = Utility.userTravels;
+    }
+
+    public void insertTravel(Travel travel) {
+        new InsertTravelAsyncTask(travelDao).execute(travel);
+
+    }
+
+    public void updateTravel(Travel travel) {
+        new UpdateTravelAsyncTask(travelDao).execute(travel);
+
+    }
+
+    public void deleteTravel(Travel travel) {
+        new DeleteTravelAsyncTask(travelDao).execute(travel);
+
+    }
+
+    public List<Travel> getAllTravels() {
+        return allTravels;
+    }
+
+    //has to be static otherwise memory leaks can happen
+    private static class InsertTravelAsyncTask extends AsyncTask<Travel, Void, Void> {
+        //call again travelDao for db operations
+        private TravelDao travelDao;
+
+        private InsertTravelAsyncTask(TravelDao travelDao) {
+            this.travelDao = travelDao;
+        }
+
+        @Override
+        protected Void doInBackground(Travel... travels) {
+            //just need to insert in the one hence the 0(first)
+            travelDao.insert(travels[0]);
+            return null;
+        }
+    }
+
+    //has to be static otherwise memory leaks can happen
+    private static class UpdateTravelAsyncTask extends AsyncTask<Travel, Void, Void> {
+        //call again travelDao for db operations
+        private TravelDao travelDao;
+
+        private UpdateTravelAsyncTask(TravelDao travelDao) {
+            this.travelDao = travelDao;
+        }
+
+        @Override
+        protected Void doInBackground(Travel... travels) {
+            //just need to insert in the one hence the 0(first)
+            travelDao.update(travels[0]);
+            return null;
+        }
+    }
+
+    //has to be static otherwise memory leaks can happen
+    private static class DeleteTravelAsyncTask extends AsyncTask<Travel, Void, Void> {
+        //call again travelDao for db operations
+        private TravelDao travelDao;
+
+        private DeleteTravelAsyncTask(TravelDao travelDao) {
+            this.travelDao = travelDao;
+        }
+
+        @Override
+        protected Void doInBackground(Travel... travels) {
+            //just need to insert in the one hence the 0(first)
+            travelDao.delete(travels[0]);
+            return null;
+        }
+    }
+}
--- app/src/main/java/com/example/tragether/ViewModel/TravelViewModel.java
+++ app/src/main/java/com/example/tragether/ViewModel/TravelViewModel.java
@@ -4,9 +4,6 @@ import android.app.Application;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
-
-
-import com.example.tragether.ViewModel.TravelRepository;
 import com.example.tragether.model.Travel;
 
 import java.util.List;
